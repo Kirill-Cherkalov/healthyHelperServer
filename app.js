9114b6a5373c230bfcf10ef56b00677e72476ace
Kirill Cherkalov <kirill.cherkalov@gmail.com>
2018-12-08 16:07:10 +0400

    added cool

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const cool = require('cool-ascii-faces')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
 const drug = require('./routes/drug.route'); // Imports routes for the products
 const app = express();
 
@@ -15,12 +19,14 @@ mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({extended: true}))
+  .use('/drug', drug)
 
-app.use('/drug', drug);
-
-let port = 1234;
-app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
-});
+  .use(express.static(path.join(__dirname, 'public')))
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
